195934d8571ff2673edda74d474c3ec7812d0e0e
Andreas Baumgartner <baumgartner.andy@hotmail.ch>
2022-09-17 02:22:33 +0200

    add gitignore

--- main.py
+++ main.py
@@ -2,11 +2,9 @@
 import matplotlib.pyplot as plt
 import keras_ocr
 
-# keras-ocr will automatically download pretrained
-# weights for the detector and recognizer.
 pipeline = keras_ocr.pipeline.Pipeline()
 
-# Get a set of three example images
+# Get a set of three two example images
 images = [
     keras_ocr.tools.read(url) for url in [
         'https://upload.wikimedia.org/wikipedia/commons/b/bd/Army_Reserves_Recruitment_Banner_MOD_45156284.jpg',
@@ -14,8 +12,6 @@ images = [
     ]
 ]
 
-# Each list of predictions in prediction_groups is a list of
-# (word, box) tuples.
 prediction_groups = pipeline.recognize(images)
 
 # Plot the predictions
@@ -23,4 +19,5 @@ fig, axs = plt.subplots(nrows=len(images), figsize=(20, 20))
 for ax, image, predictions in zip(axs, images, prediction_groups):
     keras_ocr.tools.drawAnnotations(image=image, predictions=predictions, ax=ax)
     
+# Open the Plot in a window    
 plt.show()
