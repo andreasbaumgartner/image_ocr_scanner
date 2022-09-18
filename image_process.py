846200724243d2a6e4a50dd21a5b02f6dad9133e
Andreas Baumgartner <baumgartner.andy@hotmail.ch>
2022-09-18 18:44:50 +0200

    Clean Code and defnie images_prozess

--- image_process.py
+++ image_process.py
@@ -1,6 +1,7 @@
 # imports
 import keras_ocr as keras_ocr
-import matplotlib.pyplot as plt
+
+pipeline = keras_ocr.pipeline.Pipeline()
 import config
 from config import logger
 
@@ -15,67 +16,56 @@ import fake_input
 
 
 class KerasPred:
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, urls):
+        self.urls = urls
 
     def __enter__(self):
         # Load and Check Settings
         return self
 
     def __exit__(self):
-        logger.info("KerrasMain Process exit")
         exit()  # TODO Check this function
+        logger.info("KerrasMain Process exit")
 
     def check_input(self):
-        url_count: int = InputCheck(self.url).check_multi_or_single()
-        url_health: bool = ImageUrlHealthCheck(self.url).url_pass()
+        url_count: int = InputCheck(self.urls).check_multi_or_single()
+        url_health: bool = ImageUrlHealthCheck(self.urls).url_pass()
 
         if url_count != 0 and url_health == True:
             return True
         else:
-            logger.warning(f"Input is False, {url_health} {self.url_count}")
+            logger.warning(f"Input is False, {url_health} {url_count}")
             return False
 
     def load_images(self):
-        pipeline = keras_ocr.pipeline.Pipeline()
-        url_count: int = InputCheck(self.url).check_multi_or_single()
 
-        # TODO Debug prints
-        a = self.check_input()
-        print("Check Input", a)
-        b = url_count
-        print("Url Count", b)
-
-        if self.check_input() == True and url_count == 1:
-            images = keras_ocr.tools.read(self.url)
-            prediction_groups = pipeline.recognize(images)
-            return images, prediction_groups
-
-        elif self.check_input() == True and url_count > 1:
-            print("elif triggered")
-            images = [keras_ocr.tools.read(url) for url in self.url]
+        if self.check_input() == True:
+            images: list = []
+            for url in self.urls:
+                image = keras_ocr.tools.read(url)
+                images.append(image)
             prediction_groups = pipeline.recognize(images)
             return images, prediction_groups
         else:
-            return None
+            images: list = []
+            prediction_groups = []
+            return images, prediction_groups
 
     def process_img(self):
-        print("Starting ...Processing")
-        if self.load_images() != False:
+        if self.load_images() != 0:
+
             images, prediction_groups = self.load_images()
             fig, axs = PlotImages(images).image_plot()
             for ax, image, predictions in zip(axs, images, prediction_groups):
                 keras_ocr.tools.drawAnnotations(
                     image=image, predictions=predictions, ax=ax
                 )
-            plt.show()
 
 
 # Test Cases
-url = [
-    "https://images.unsplash.com/photo-1659599746931-09cff34dd307?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80"
+test = [
+    "https://images.unsplash.com/photo-1659599746931-09cff34dd307?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80",
+    "https://images.unsplash.com/photo-1659976400255-d5cca2c2234a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80",
 ]
-
-
-a = KerasPred(url).load_images()
+a = KerasPred(test).load_images()
 print(a)
--- url_image.py
+++ url_image.py
@@ -12,25 +12,42 @@ class ImageUrlHealthCheck:
     Function url_pass return True if the Health Check is right
     """
 
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, urls):
+        self.urls = urls
 
     def check_url(self):
-        response = requests.get(self.url)
-        if response.status_code == 200:
+        print("Starting Check Url")
+        status_code = []
+        for url in self.urls:
+            try:
+                response = requests.get(url)
+                if response.status_code == 200:
+                    status_code.append(True)
+                else:
+                    logger.warning(f"Returned Status Code {response.status_code}")
+                    status_code.append(False)
+            except:
+                return False
+        if False not in status_code:
             return True
-        else:
-            logger.warning(f"Returned Status Code {response.status_code}")
-            return False
 
     def check_content(self, image_formats=config.IMAGE_FORMATS):
-        response = requests.head(self.url)
-        if response.headers["content-type"] in image_formats:
+        print("Starting Check...")
+        status_list = []
+        for url in self.urls:
+            try:
+                response = requests.head(url)
+                if response.headers["content-type"] in image_formats:
+                    status_list.append(True)
+            except:
+                status_list.append(False)
+                continue
+
+        if False not in status_list:
             return True
-        else:
-            return False
 
     def url_pass(self):
+        print("Starting Url_pAss..")
         if self.check_url() == True and self.check_content() == True:
             return True
         else:
@@ -39,7 +56,11 @@ class ImageUrlHealthCheck:
 
 
 # Test Cases Function
-# test = "https://images.unsplash.com/photo-1659599746931-09cff34dd307?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80"
-#
-# s = ImageUrlHealthCheck(test).url_pass()
-# print(s)
+test = [
+    "https://images.unsplash.com/photo-1659599746931-09cff34dd307?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80",
+    "https://images.unsplash.com/photo-1659976400255-d5cca2c2234a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80",
+]
+
+
+s = ImageUrlHealthCheck(test).url_pass()
+print(s)
